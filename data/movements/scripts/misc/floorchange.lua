f72e8a68d86a185dfd20468d7eec77d93de99a98
ErikasKontenis <e.kontenis@gmail.com>
2019-11-21 18:28:11 +0200

    first commit to successful liberty bay import to map

--- data/movements/scripts/misc/floorchange.lua
+++ data/movements/scripts/misc/floorchange.lua
@@ -70,6 +70,7 @@ local list = {
 	[2194] = {x = 1, y = -1, z = -1},
 	[2196] = {x = 1, y = 1, z = -1},
 	[2198] = {x = -1, y = 1, z = -1},
+	[5258] = {x = 0, y = -1, z = -1},
 }
 
 function onStepIn(creature, item, position, fromPosition)
