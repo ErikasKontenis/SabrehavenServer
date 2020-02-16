5f9d23c1279a0c8542b85aa90bb35b31d4806468
ErikasKontenis <e.kontenis@gmail.com>
2020-02-16 19:02:09 +0200

    make sound for food if 790

--- data/actions/scripts/misc/food.lua
+++ data/actions/scripts/misc/food.lua
@@ -1,3 +1,48 @@
+local foods = {
+	[3250] = "Crunch.", -- the carrot of doom
+	[3577] = "Munch.", -- meat
+	[3578] = "Munch.", -- fish
+	[3579] = "Mmmm.", -- salmon
+	[3580] = "Munch.", -- fish
+	[3581] = "Gulp.", -- shrimp
+	[3582] = "Chomp.", -- ham
+	[3583] = "Chomp.", -- dragon ham
+	[3584] = "Yum.", -- pear
+	[3585] = "Yum.", -- red apple
+	[3586] = "Yum.", -- orange
+	[3587] = "Yum.", -- banana
+	[3588] = "Yum.", -- blueberry
+	[3589] = "Slurp.", -- coconut
+	[3590] = "Yum.", -- cherry
+	[3591] = "Yum.", -- strawberry
+	[3592] = "Yum.", -- grapes
+	[3593] = "Yum.", -- melon
+	[3594] = "Munch.", -- pumpkin
+	[3595] = "Crunch.", -- carrot
+	[3596] = "Munch.", -- tomato
+	[3597] = "Crunch.", -- corncob
+	[3598] = "Crunch.", -- cookie
+	[3599] = "Munch.", -- candy cane
+	[3600] = "Crunch.", -- bread
+	[3601] = "Crunch.", -- roll
+	[3602] = "Crunch.", -- brown bread
+	[3606] = "Gulp.", -- egg
+	[3607] = "Smack.", -- cheese
+	[3723] = "Munch.", -- white mushroom
+	[3724] = "Munch.", -- red mushroom
+	[3725] = "Munch.", -- brown mushroom
+	[3726] = "Munch.", -- orange mushroom
+	[3727] = "Munch.", -- wood mushroom
+	[3728] = "Munch.", -- dark mushroom
+	[3729] = "Munch.", -- some mushrooms
+	[3730] = "Munch.", -- some mushrooms
+	[3731] = "Munch.", -- fire mushroom
+	[3732] = "Munch.", -- green mushroom
+	[5096] = "Yum.", -- mango
+	[5678] = "Gulp.", -- tortoise egg
+	[6125] = "Gulp." -- tortoise egg from Nargor
+}
+
 function onUse(player, item, fromPosition, target, toPosition)
 	local itemType = ItemType(item:getId())
 	local condition = player:getCondition(CONDITION_REGENERATION, CONDITIONID_DEFAULT)
@@ -6,6 +51,10 @@ function onUse(player, item, fromPosition, target, toPosition)
 	else
 		player:feed(itemType:getNutrition() * 12)
 		item:remove(1)
+		
+		if configManager.getNumber(configKeys.CLIENT_VERSION) >= 790 then
+			player:say(foods[item:getId()] or "Munch.", TALKTYPE_MONSTER_SAY)
+		end
 	end
 	
 	return true
